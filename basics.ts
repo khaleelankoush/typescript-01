4681fec0a74879d70bca4ba170d54ecc861e3137
khaleelankoush <khaleel.ankoush@gmail.com>
2022-09-03 19:46:50 +0300

    edits

--- basics.ts
+++ basics.ts
@@ -9,3 +9,63 @@ let age: number;
 age = 12;
 
 let userName: string;
+
+userName = 'Khaleel';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+type Person = {
+    name: string;
+    age: number;
+};
+
+let person: Person;
+
+person = {
+    name: 'Khaleel',
+    age: 25
+};
+
+// person = {
+//     isEmployee: true,
+// };
+
+let people: Person[];
+
+// Type inference
+
+let course: string | number = 'React - The Complete Guide';
+
+course = 12245;
+
+// Functions & types
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function print(value: any) {
+    console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+    const newArray = [value, ...array];
+    return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+// updatedArray[0].split('');
